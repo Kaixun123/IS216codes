dac94314ef5b23d2a8e069565bea9f8bb8b4c781
Juay Kai Xun <juaykaixun@hotmail.com>
2023-10-12 22:10:24 +0800

    lab test prac

--- Lab test prac/Sample_MiniLabTest2/Sample_MiniLabTest2/resources/Q2/slideshow.js
+++ Lab test prac/Sample_MiniLabTest2/Sample_MiniLabTest2/resources/Q2/slideshow.js
@@ -14,7 +14,68 @@ function display_default() {
     // YOUR CODE GOES HERE
     // YOU MAY ADD MORE FUNCTION(S) IF YOU WISH
 
-    
+
+    const url = "http://localhost/MLT/Q2/api/info.php";
+    axios.get(url).then(response => {
+        console.log(response.data);
+        var military_statistics = response.data.military_statistics;
+        htmlstr = "";
+        var active = " active";
+        var firstCountryPassed = false;
+        console.log(military_statistics);
+
+        for(country in military_statistics){
+            console.log(country);
+            
+            var countryDetails = military_statistics[country];
+            var countryFlag = countryDetails["flag_relative_url"];
+            var countryPres = countryDetails["head_of_state"];
+
+            htmlstr += `
+           <div class="carousel-item${active}" data-bs-interval="3000">
+                <img src="${countryFlag}" class="d-block w-100">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${country}</h5>
+                    <p>${countryPres}</p>
+                </div>
+            </div>
+           `; 
+
+           if(!firstCountryPassed){
+             firstCountryPassed = true;
+             active="";
+           }
+        }
+
+        // for(country in military_statistics){
+        //    console.log(country)
+
+        //    var countryDetails = military_statistics[country];
+
+        //    if(!firstCountryPassed){
+        //      firstCountryPassed = true;
+        //      active = "";
+        //    }
+
+        //    console.log(countryDetails["flag_relative_url"]);
+        //    htmlstr += `
+        //    <div class="carousel-item${active}">
+        //         <img src="${countryDetails["flag_relative_url"]}" class="d-block w-100">
+        //         <div class="carousel-caption d-none d-md-block">
+        //             <h5>${countryDetails}</h5>
+        //             <p>${countryDetails["head_of_state"]}</p>
+        //         </div>
+        //     </div>
+        //    `; 
+        // }
+         console.log(htmlstr);
+         document.getElementById("imageCarousel").innerHTML = htmlstr;
+    }).catch(error => {
+        console.log(error.message);
+        
+    });
+
+
     console.log("===[END] display_default() ===")
 
 }
--- Lab test prac/Sample_MiniLabTest2/Sample_MiniLabTest2/resources/Q2/table.js
+++ Lab test prac/Sample_MiniLabTest2/Sample_MiniLabTest2/resources/Q2/table.js
@@ -13,6 +13,48 @@ function display_default() {
     // YOUR CODE GOES HERE
     // YOU MAY ADD MORE FUNCTION(S) IF YOU WISH
 
+    const url = "http://localhost/MLT/Q2/api/info.php";
+    axios.get(url).then(response => {
+        console.log(response.data);
+        var military_statistics = response.data.military_statistics;
+        htmlstr = "";
+        console.log(military_statistics);
+        
+
+        for(country in military_statistics){
+            console.log(country);
+            var countryDetails = military_statistics[country];
+            var countryPres = countryDetails["head_of_state"];
+            var countryPopulation = countryDetails["personnel"]["total_population"];
+            var countryReserves = countryDetails["personnel"]["total_fit_for_service"];
+            var fitForServicePercent = (countryReserves/countryPopulation) * 100;
+
+            htmlstr += `
+            <tr>
+                <th scope="row">${country}</th>
+                <td>${countryPres}</td>
+                <td>${countryPopulation}</td>
+            
+           `; 
+
+            if(fitForServicePercent.toFixed(1)<30){
+                htmlstr += `<td class="font-okay">${fitForServicePercent.toFixed(1)}</td>`;
+            }else if(fitForServicePercent.toFixed(1) >= 30 && fitForServicePercent.toFixed(1) < 40){
+                htmlstr += `<td class="font-omg">${fitForServicePercent.toFixed(1)}</td>`;
+            }else{
+                htmlstr += `<td class="font-holy-moly">${fitForServicePercent.toFixed(1)}</td>`;
+            }
+
+            htmlstr+=`</tr>`;
+        }
+
+         console.log(htmlstr);
+         document.getElementById("countryTable").innerHTML = htmlstr;
+    }).catch(error => {
+        console.log(error.message);
+        
+    });
+
 
     console.log("===[END] display_default() ===")
 
